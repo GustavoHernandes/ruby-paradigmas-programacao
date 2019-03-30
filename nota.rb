f1772c46bb1ae0060428afe3b172aa957f4d6cd9
Tales peres <tales.oliveira@alunos.fho.edu.br>
2019-03-30 10:38:35 -0300

    Adicionado calculo da Presença

--- nota.rb
+++ nota.rb
@@ -13,33 +13,38 @@ Mb1 = gets.to_f
 # Calcula A1
 A1 = 0.7*P1 + 0.2*Ma1 + 0.1*Mb1
 
-print 'Digite a nota de "Prova 2:" '
-Prova2 = gets.to_f
+print 'Digite a nota de P2(Prova 2): '
+P2 = gets.to_f
 
-print 'Digite a nota de "Exercicios 2": '
-Ex2 = gets.to_f
-
-# Calcula P2
-P2 = 0.5*Prova2 + 0.2*Ex2
-
-print "Digite a nota de Ma2(Trabalho): "
+print 'Digite a nota de Ma2(Exercicios 2): '
 Ma2 = gets.to_f
 
 print "Digite a nota da Mb2(Atividades 2): "
 Mb2 = gets.to_f
 
 # Calcula A2
-A2 = P2 + 0.2*Ma2 + 0.1*Mb2 
+A2 = 0.7*P2 + 0.2*Ma2 + 0.1*Mb2 
+
+print "Digite a quantide de aulas: "
+qtdAulas = gets.to_f 
+
+print "Digite a quantide de faltas: "
+qtdFaltas = gets.to_f 
 
 # Calcula Nota Final
 NotaFinal = ((A1 + 2*A2)/3).round(2)
 
-if NotaFinal >= 5.0
+Presenca = 100 - ((qtdFaltas/qtdAulas)*100)
+
+if NotaFinal >= 5.0 and Presenca >= 75
 	puts "\r\nAprovado!"
-elsif NotaFinal >= 3.0 and NotaFinal < 5.0
-	puts "\r\nRE"
+elsif NotaFinal >= 3.0 and NotaFinal < 5.0 and Presenca >= 75
+	puts "\r\nRecuperação!"
+elsif NotaFinal < 3.0 and Presenca >= 75
+	puts "\r\nReprovado por nota!"
 else
-	puts "\r\nReprovado!"
+	puts "\r\nReprovado por falta!"
 end
 
 puts "Sua nota final é #{NotaFinal}"
+puts "Presença #{Presenca}%"
